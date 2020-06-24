f5ef8564163dafc1d6071cd6d12e60565732c1e6
melissa.kouadio <melissa.kouadio@veone.net>
2020-06-24 17:30:44 +0200

    plateforme immobilier en symfony 4

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\Ad;
 use App\Form\AdFormType;
 use App\Repository\AdRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -26,13 +30,22 @@ class AdController extends AbstractController
      * @Route("/ads/new", name="create_ads")
      *@return Response;
      */
-     
-    public function create()
+    public function create(Request $request, ManagerRegistry $managerRegistry)
     {
         $ad = new Ad();
         $form=$this->createForm(AdFormType::class, $ad);
+        $form->handleRequest($request);
 
-        return $this->render('ad/new.html.twig',[
+        if($form->isSubmitted() && $form->isValid())
+            {
+                $manage = $managerRegistry->getManager();
+                $manage->persist($ad);
+                $manage->flush();
+                $this->redirectToRoute('single_ad',[
+                'slug' => $ad->getSlug(),
+            ]);
+            }
+         return $this->render('ad/new.html.twig',[
             'form'=> $form->createView()
         ]);
     }
@@ -42,14 +55,11 @@ class AdController extends AbstractController
      * @Route("/ads/{slug}", name="single_ad")
      *@return Response;
      */
-     
+
     public function show(Ad $singleAd)
     {
         return $this->render('ad/show.html.twig',[
             'singleAd'=> $singleAd,
         ]);
     }
-
-   
-    
 }
--- src/Form/AdFormType.php
+++ src/Form/AdFormType.php
@@ -6,20 +6,38 @@ use App\Entity\Ad;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class AdFormType extends AbstractType
 {
+ /**
+  * configuration de base des champs de notre formulaire
+  *
+  * @param string $label
+  * @param string placeholder
+  * @return array
+  */
+ private function configForm($label, $placeholder)
+ {
+    return [
+        'label'=>$label,
+        'attr' => ['placeholder'=> $placeholder ]];
+ }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('slug')
-            ->add('price')
-            ->add('introduction')
-            ->add('content')
-            ->add('coverImage')
-            ->add('rooms')
-        ;
+            ->add('title',TextType::class, $this->configForm("titre" ,"Ajouter un titre à l'annonce" ))
+            ->add('introduction',TextareaType::class, $this->configForm("Résumé", "Ajouter un résumé à l'annonce"))
+            ->add('coverImage',UrlType::class, $this->configForm("Image", "Ajouter ule lien d'une image de l'annonce"))
+            ->add('content',TextareaType::class, $this->configForm("Description", "Ajouter une description à l'annonce"))
+            ->add('slug', UrlType::class, $this->configForm("Adresse web", "Ajouter le site web de l'annonce"))
+            ->add('rooms',IntegerType::class, $this->configForm("Nombre de pièce", "Ajouter le nombre de pièce "))
+            ->add('price', MoneyType::class, $this->configForm("Montant", "Ajouter le montant de cette annonce "));
     }
     public function configureOptions(OptionsResolver $resolver)
     {
